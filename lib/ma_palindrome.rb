4f4b0eb5f8d6de94acf91dcf5dc37c776f94ee01
PhilipMa2 <ma_philip2000@hotmail.com>
2020-08-11 10:06:04 -0500

    Initialize repository

---
+++ lib/ma_palindrome.rb
@@ -0,0 +1,6 @@
+require "ma_palindrome/version"
+
+module MaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ma_palindrome/version.rb
@@ -0,0 +1,3 @@
+module MaPalindrome
+  VERSION = "0.1.0"
+end
